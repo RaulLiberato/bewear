5af4b888a6978b9f0d21918a822d50e1f61f44f1
RaulLiberato <ctt.raul.liberato@gmail.com>
2025-08-08 12:04:54 -0300

    feat: ensure cart product quantity can be incresed

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -19,7 +19,7 @@ import {
 import CartItem from "./cart-item";
 
 export const Cart = () => {
-  const { data: cart, isPending: cartIsLoading } = useQuery({
+  const { data: cart } = useQuery({
     queryKey: ["cart"],
     queryFn: () => getCart(),
   });
@@ -43,6 +43,7 @@ export const Cart = () => {
                   <CartItem
                     key={item.id}
                     id={item.id}
+                    productVariantId={item.productVariant.id}
                     productName={item.productVariant.product.name}
                     productVariantName={item.productVariant.name}
                     productVariantImageUrl={item.productVariant.imageUrl}
